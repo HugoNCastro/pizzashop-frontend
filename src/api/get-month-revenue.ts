974c988cc0c023a1a429ed05b759f4c52b144d5d
HugoNCastro <hugonaaman@gmail.com>
2025-02-15 15:21:50 -0300

    Ajusta nomenclatura de tipagem na rota de receita mensal
    Cria rota para obter produtos popular e mostra dados no componente

--- src/api/get-month-revenue.ts
+++ src/api/get-month-revenue.ts
@@ -1,12 +1,12 @@
 import { api } from "@/lib/axios";
 
-export interface GetMonthRevenuetResponse {
+export interface GetMonthRevenueResponse {
   receipt: number;
   diffFromLastMonth: number;
 }
 
 export async function getMonthRevenue() {
-  const response = await api.get<GetMonthRevenuetResponse>('/metrics/month-receipt');
+  const response = await api.get<GetMonthRevenueResponse>('/metrics/month-receipt');
 
   return response.data
 }
